ec958ed67abe265e1414b8fc85f2b81d2339256c
tomykaira <tomykaira@gmail.com>
2013-02-27 12:25:05 +0900

    Make more efficient and simple

--- etc/rbenv.d/exec/gemset.bash
+++ etc/rbenv.d/exec/gemset.bash
@@ -6,16 +6,11 @@ else
   RBENV_GEMSET_ROOT="$(rbenv-prefix)/gemsets"
 fi
 
-gemsets=`rbenv-gemset active 2>/dev/null`
-
-# to avoid infinite loop by evaluating recursively
-if [ -n "$gemsets" ]; then
-  DEFAULT_GEM_PATH=`RBENV_GEMSETS=" " gem env gempath`
-fi
-
-for gemset in $gemsets; do
+for gemset in $(rbenv-gemset active 2>/dev/null); do
   if [ $gemset = "__DEFAULT__" ]; then
-    GEM_PATH="$GEM_PATH:$DEFAULT_GEM_PATH"
+    # to avoid infinite loop by evaluating recursively
+    default_gem_path=`RBENV_GEMSETS=" " gem env gempath`
+    GEM_PATH="$GEM_PATH:$default_gem_path"
   else
     path="${RBENV_GEMSET_ROOT}/$gemset"
     if [ -z "$GEM_HOME" ]; then
