3acba57303873fab5ef1d9a281c486642ff4cc92
tomykaira <tomykaira@gmail.com>
2013-02-27 12:09:21 +0900

    Add special __DEFAULT__ gemset to include all default gems
    "Global" gemset is not used when no gemset is active.
    A rbenv user usually installs gems into the default directory, not "global" gemset.
    This option make it easy to reuse these gems in the default directory.
    
    Current implementation get gempath by
    
        RBENV_GEMSETS=" " gem env gempath
    
    but it is ugly.  Please modify if there is a better way

--- etc/rbenv.d/exec/gemset.bash
+++ etc/rbenv.d/exec/gemset.bash
@@ -6,12 +6,21 @@ else
   RBENV_GEMSET_ROOT="$(rbenv-prefix)/gemsets"
 fi
 
-for gemset in $(rbenv-gemset active 2>/dev/null); do
-  path="${RBENV_GEMSET_ROOT}/$gemset"
-  if [ -z "$GEM_HOME" ]; then
-    GEM_HOME="$path"
-    GEM_PATH="$path"
+gemsets=`rbenv-gemset active 2>/dev/null`
+
+# to avoid infinite loop by evaluating recursively
+if [ -n "$gemsets" ]; then
+  DEFAULT_GEM_PATH=`RBENV_GEMSETS=" " gem env gempath`
+fi
+
+for gemset in $gemsets; do
+  if [ $gemset = "__DEFAULT__" ]; then
+    GEM_PATH="$GEM_PATH:$DEFAULT_GEM_PATH"
   else
+    path="${RBENV_GEMSET_ROOT}/$gemset"
+    if [ -z "$GEM_HOME" ]; then
+      GEM_HOME="$path"
+    fi
     GEM_PATH="$GEM_PATH:$path"
   fi
 done
