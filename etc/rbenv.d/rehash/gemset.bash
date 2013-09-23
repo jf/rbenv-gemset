2cd80edc51831405f87f851927e20786d6f3a93d
Jeffrey 'jf' Lim <jfs.world@gmail.com>
2013-09-23 23:42:46 +0800

    Fix #48; bump version up to 0.4.1

--- etc/rbenv.d/rehash/gemset.bash
+++ etc/rbenv.d/rehash/gemset.bash
@@ -1,9 +1,21 @@
 shopt -s nullglob
+
+RBENV_GEMSET_DIR="$(dirname "$(rbenv-gemset file 2>/dev/null)" 2>/dev/null)"
+PROJECT_GEMSET_LIST="${RBENV_ROOT}/versions/$(rbenv-version-name)/gemsets/.project-gemsets"
+project_gemset='\..+'
 OLDIFS="$IFS"
 IFS=$' \t\n'
-gemset_bin=(${RBENV_ROOT}/versions/*/gemsets/*/bin/*)
+for gemset in $(rbenv-gemset active 2>/dev/null); do
+  if [[ $gemset =~ $project_gemset ]]; then
+    mkdir -p "$PROJECT_GEMSET_LIST"
+    gemset=${RBENV_GEMSET_DIR}/$gemset
+    link=${PROJECT_GEMSET_LIST}/${gemset////__}
+    ln -sf "$gemset" "$link"
+  fi
+done
 IFS="$OLDIFS"
 
 cd "$SHIM_PATH"
-make_shims ${gemset_bin[@]}
+gemset_bin=(${RBENV_ROOT}/versions/*/gemsets/*/bin/*)
+make_shims "${gemset_bin[@]}" "${RBENV_ROOT}/versions/*/gemsets/.project-gemsets/*/bin/*"
 cd "$CUR_PATH"
--- etc/rbenv.d/which/gemset.bash
+++ etc/rbenv.d/which/gemset.bash
@@ -4,13 +4,23 @@ else
   RBENV_GEMSET_ROOT="$(rbenv-prefix)/gemsets"
 fi
 
+RBENV_GEMSET_DIR="$(dirname "$(rbenv-gemset file 2>/dev/null)" 2>/dev/null)"
+project_gemset='\..+'
 OLDIFS="$IFS"
 IFS=$' \t\n'
 for gemset in $(rbenv-gemset active 2>/dev/null); do
-  command="${RBENV_GEMSET_ROOT}/${gemset}/bin/$RBENV_COMMAND"
-  if [ -x "$command" ]; then
-    RBENV_COMMAND_PATH="$command"
-    break
+  if [[ $gemset =~ $project_gemset ]]; then
+    command="${RBENV_GEMSET_DIR}/${gemset}/bin/$RBENV_COMMAND"
+    if [ -x "$command" ]; then
+      RBENV_COMMAND_PATH="$command"
+      break
+    fi
+  else
+    command="${RBENV_GEMSET_ROOT}/${gemset}/bin/$RBENV_COMMAND"
+    if [ -x "$command" ]; then
+      RBENV_COMMAND_PATH="$command"
+      break
+    fi
   fi
 done
 IFS="$OLDIFS"
