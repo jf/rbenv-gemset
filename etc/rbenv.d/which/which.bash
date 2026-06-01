7e50c7dad671b7b777005d4c32bf7ff3169a0a53
Jeffrey 'jf' Lim <jf@users.noreply.github.com>
2026-06-02 02:17:17 +0800

    optimization: - update `rbenv-gemset-active` to accept pre-found _rbenv_gemsets_file location (all hooks call `rbenv-gemset-file`, then `rbenv-gemset-active`, _always in that order!_) - optimize-update which hook (also resolve #106)

--- etc/rbenv.d/which/which.bash
+++ etc/rbenv.d/which/which.bash
@@ -1,22 +1,24 @@
-if [ "$(rbenv-version-name)" = "system" ]; then
-  RBENV_GEMSET_ROOT="$RBENV_GEMSET_SYSTEM_ROOT"
-else
-  RBENV_GEMSET_ROOT="$(rbenv-prefix)/gemsets"
-fi
+# dont bother carrying on if `rbenv version` is "system"; the codebase has not supported this for a long time now!
+[[ $RBENV_VERSION = "system" ]] && return
 
-RBENV_GEMSET_DIR="$(dirname "$(rbenv-gemset file 2>/dev/null)" 2>/dev/null)"
-project_gemset='^\..+'
+# EARLY RETURN #2: no gemset file = no work to do!
+_rbenv_gemsets_file="$( rbenv-gemset-file 2>/dev/null )"
+[[ -z $_rbenv_gemsets_file ]] && return
+
+_rbenv_gemsets_container="$( rbenv-prefix )/gemsets"
+_project_dir=${_rbenv_gemsets_file%/*}
+
 OLDIFS="$IFS"
 IFS=$' \t\n'
-for gemset in $(rbenv-gemset active 2>/dev/null); do
-  if [[ $gemset =~ $project_gemset ]]; then
-    command="${RBENV_GEMSET_DIR}/${gemset}/bin/$RBENV_COMMAND"
+for gemset in $( _rbenv_gemsets_file=$_rbenv_gemsets_file rbenv-gemset-active 2>/dev/null ); do
+  if [[ $gemset =~ ^\..+ ]]; then
+    command="$_project_dir/$gemset/bin/$RBENV_COMMAND"
     if [ -x "$command" ]; then
       RBENV_COMMAND_PATH="$command"
       break
     fi
   else
-    command="${RBENV_GEMSET_ROOT}/${gemset}/bin/$RBENV_COMMAND"
+    command="$_rbenv_gemsets_container/$gemset/bin/$RBENV_COMMAND"
     if [ -x "$command" ]; then
       RBENV_COMMAND_PATH="$command"
       break
